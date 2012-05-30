bab9bb495a4f03209ac262b4f17af700d06abaf4
TOGoS <togos00@gmail.com>
2012-05-29 19:35:32 -0500

    Renamed 'data' to 'buffer', and 'payload' to 'data' in TCPSegment to be more in line with terminology in the TCP article on Wikipedia.

--- src/togos/icmpresponder/packet/TCPSegment.java
+++ src/togos/icmpresponder/packet/TCPSegment.java
@@ -23,8 +23,9 @@ public class TCPSegment extends SimpleByteChunk
 	
 	public int optionsOffset = 0;
 	public int optionsSize = 0;
-	public int payloadOffset = 0;
-	public int payloadSize = 0;
+	/** Note that this offset is in *bytes*, not 32-bit words as in the actual header data */ 
+	public int dataOffset = 0;
+	public int dataSize = 0;
 	
 	public TCPSegment( byte[] data, int offset, int size, IPPacket p, String note ) {
 		super( data, offset, size );
@@ -50,12 +51,12 @@ public class TCPSegment extends SimpleByteChunk
 	 * make sure packets are properly formed.
 	 */
 	public static int calculateChecksum( TCPSegment m ) {
-		byte[] checksumBuffer = new byte[60 + m.optionsSize + m.payloadSize];
+		byte[] checksumBuffer = new byte[60 + m.optionsSize + m.dataSize];
 		// Not yet implemented for IP4 packets
 		IP6Packet ip6p = (IP6Packet)m.ipPacket;
 		ByteUtil.copy( ip6p.getBuffer(), ip6p.getSourceAddressOffset()       , checksumBuffer,  0, 16 );
 		ByteUtil.copy( ip6p.getBuffer(), ip6p.getDestinationAddressOffset()  , checksumBuffer, 16, 16 );
-		ByteUtil.encodeInt32( TCP_HEADER_SIZE + m.optionsSize + m.payloadSize, checksumBuffer,     32 ); // 'TCP Length'
+		ByteUtil.encodeInt32( TCP_HEADER_SIZE + m.optionsSize + m.dataSize, checksumBuffer,     32 ); // 'TCP Length'
 		ByteUtil.encodeInt32( TCP_PROTOCOL_NUMBER, checksumBuffer, 36 ); // 'next header'
 		
 		// A simpler way (what ICMP6Message.calculateIcmp6Checksum( m.ipPacket ) would do):
@@ -63,7 +64,7 @@ public class TCPSegment extends SimpleByteChunk
 		
 		ByteUtil.copy( m.getBuffer(), m.getOffset(), checksumBuffer, 40, 20 ); // src/dest port, seq/ack num, falgs, windowSize, checksum, and urgent pointer
 		ByteUtil.copy( m.getBuffer(), m.optionsOffset, checksumBuffer, 60, m.optionsSize );
-		ByteUtil.copy( m.getBuffer(), m.payloadOffset, checksumBuffer, 60 + m.optionsSize, m.payloadSize );
+		ByteUtil.copy( m.getBuffer(), m.dataOffset, checksumBuffer, 60 + m.optionsSize, m.dataSize );
 		
 		return (int)InternetChecksum.checksum(checksumBuffer);
 	}
@@ -72,68 +73,68 @@ public class TCPSegment extends SimpleByteChunk
 		byte[] sourceAddressBuffer, int sourceAddressOffset, int sourcePort,
 		byte[] destAddressBuffer, int destAddressOffset, int destPort,
 		int seqNum, int ackNum, int flags, int windowSize,
-		byte[] payloadBuffer, int payloadOffset, int payloadSize
+		byte[] dataBuffer, int dataOffset, int dataSize
 	) {
 		IP6Packet p = IP6Packet.create(
 			sourceAddressBuffer, sourceAddressOffset,
 			destAddressBuffer, destAddressOffset,
-			TCP_PROTOCOL_NUMBER, 255, TCP_HEADER_SIZE + payloadSize
+			TCP_PROTOCOL_NUMBER, 255, TCP_HEADER_SIZE + dataSize
 		);
-		byte[] data = p.getBuffer();
+		byte[] buffer = p.getBuffer();
 		int offset = p.getPayloadOffset();
 		
-		ByteUtil.encodeInt16( sourcePort, data, offset+0 );
-		ByteUtil.encodeInt16( destPort, data, offset+2 );
-		ByteUtil.encodeInt32( seqNum, data, offset+4 );
-		ByteUtil.encodeInt32( ackNum, data, offset+8 );
+		ByteUtil.encodeInt16( sourcePort, buffer, offset+0 );
+		ByteUtil.encodeInt16( destPort, buffer, offset+2 );
+		ByteUtil.encodeInt32( seqNum, buffer, offset+4 );
+		ByteUtil.encodeInt32( ackNum, buffer, offset+8 );
 		
 		int dataOffsetWords = 5; // i.e. 20 bytes
 		int falgs = (dataOffsetWords << 12) | (flags & 0x1F);
-		ByteUtil.encodeInt16( falgs, data, offset+12 );
+		ByteUtil.encodeInt16( falgs, buffer, offset+12 );
 		
-		ByteUtil.encodeInt16( windowSize, data, offset+14 );
+		ByteUtil.encodeInt16( windowSize, buffer, offset+14 );
 		
 		// Calculate checksum
 		// Note: could also just ICMP6Message.calculateIcmp6Checksum( m.ipPacket ) after putting the packet together...
-		byte[] checksumBuffer = new byte[60 + payloadSize];
+		byte[] checksumBuffer = new byte[60 + dataSize];
 		ByteUtil.copy( sourceAddressBuffer, sourceAddressOffset, checksumBuffer,  0, 16 );
 		ByteUtil.copy(   destAddressBuffer,   destAddressOffset, checksumBuffer, 16, 16 );
-		ByteUtil.encodeInt32( TCP_HEADER_SIZE + payloadSize, checksumBuffer, 32 ); // 'TCP Length'
+		ByteUtil.encodeInt32( TCP_HEADER_SIZE + dataSize, checksumBuffer, 32 ); // 'TCP Length'
 		checksumBuffer[39] = TCP_PROTOCOL_NUMBER; // 'next header'
-		ByteUtil.copy( data, offset, checksumBuffer, 40, 16 ); // src/dest port, seq/ack num, falgs, windowSize
+		ByteUtil.copy( buffer, offset, checksumBuffer, 40, 16 ); // src/dest port, seq/ack num, falgs, windowSize
 		// Leave checksum and 'urgent pointer' zero
 		// Assuming no options.
-		ByteUtil.copy( payloadBuffer, payloadOffset, checksumBuffer, 60, payloadSize );
+		ByteUtil.copy( dataBuffer, dataOffset, checksumBuffer, 60, dataSize );
 		long checksum = InternetChecksum.checksum(checksumBuffer);
 		
-		ByteUtil.encodeInt16( (short)checksum, data, offset+16 );
+		ByteUtil.encodeInt16( (short)checksum, buffer, offset+16 );
 		
 		return TCPSegment.parse( p );
 	}
 
 	
-	public static TCPSegment parse( byte[] data, int offset, int size, IPPacket p ) {
-		if( size < 20 ) return new TCPSegment( data, offset, size, p, "TCP message size must be >= 20, but is only "+size);
+	public static TCPSegment parse( byte[] buffer, int offset, int size, IPPacket p ) {
+		if( size < 20 ) return new TCPSegment( buffer, offset, size, p, "TCP message size must be >= 20, but is only "+size);
 		
-		TCPSegment m = new TCPSegment( data, offset, size, p );
-		m.sourcePort = ByteUtil.decodeUInt16( data, offset+0 );
-		m.destPort   = ByteUtil.decodeUInt16( data, offset+2 );
-		m.sequenceNumber = ByteUtil.decodeInt32( data, offset+4 );
-		m.ackNumber = ByteUtil.decodeInt32( data, offset+8 );
-		int falgs = ByteUtil.decodeUInt16( data, offset+12 );
+		TCPSegment m = new TCPSegment( buffer, offset, size, p );
+		m.sourcePort = ByteUtil.decodeUInt16( buffer, offset+0 );
+		m.destPort   = ByteUtil.decodeUInt16( buffer, offset+2 );
+		m.sequenceNumber = ByteUtil.decodeInt32( buffer, offset+4 );
+		m.ackNumber = ByteUtil.decodeInt32( buffer, offset+8 );
+		int falgs = ByteUtil.decodeUInt16( buffer, offset+12 );
 		m.flags = falgs & 0xFFF;
 		int dataOffset = ((falgs >> 12) & 0xF) << 2;
 		if( dataOffset >= 20 || dataOffset < size ) {
 			m.optionsOffset = offset + 20;
 			m.optionsSize = dataOffset - 20;
-			m.payloadOffset = offset + dataOffset;
-			m.payloadSize = size - dataOffset;
+			m.dataOffset = offset + dataOffset;
+			m.dataSize = size - dataOffset;
 			m.wellFormed = true;
 		} else {
 			m.wellFormed = false;
 			m.note = "Data offset ("+dataOffset+" bytes) is out of range";
 		}
-		m.checksum = ByteUtil.decodeUInt16( data, offset+16 );
+		m.checksum = ByteUtil.decodeUInt16( buffer, offset+16 );
 		return m;
 	}
 	
@@ -149,7 +150,7 @@ public class TCPSegment extends SimpleByteChunk
 			" ackNum="+ackNumber+
 			" flags=0x"+Integer.toHexString(flags)+
 			" checksum="+checksum+
-			" payloadSize="+payloadSize;
+			" payloadSize="+dataSize;
 			
 	}
 }
